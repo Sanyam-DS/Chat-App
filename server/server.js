8521dd7fd82a859aea5e0b3993cd9d5621a11512
Sanyam Sharma <sanyamsharma170@gmail.com>
2025-11-18 10:02:53 +0530

    Make space on cloudinary and Create routes for user

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 //create Express app and HTTP server
 const app = express();
@@ -12,8 +13,10 @@ const server = http.createServer(app);
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
-app.use("/api/status", (req,res)=> res.send("Server is Live"));
 
+//Routes setup
+app.use("/api/status", (req,res)=> res.send("Server is Live"));
+app.use("/api/auth", userRouter);
 
 //Connect to MongoDB
 await connectDB();
